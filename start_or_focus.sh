3a4ba973d9384b574fd9899209471376d5389700
StephanMoeller <stephanryer@hotmail.com>
2024-09-05 23:40:23 +0200

    new binds

--- start_or_focus.sh
+++ start_or_focus.sh
@@ -3,10 +3,11 @@ echo "$1"
 workspace="$1"
 windowname="$2"
 startcommand="$3"
+multi="$4"
 
 hyprctl dispatch workspace $workspace
 
-# Check if Discord is running
+# Check if program is running
 if hyprctl clients | grep class | grep $windowname > /dev/null; then
     echo "focused"
     # If Discord is running, find the window and focus it
