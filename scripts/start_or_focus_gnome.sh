7784cb967dc9ff94fa5ff3ddc1f0c7f66356ffc0
StephanMoeller <stephanryer@hotmail.com>
2025-01-27 14:29:37 +0100

    changes to hyprland and scripts folder

---
+++ scripts/start_or_focus_gnome.sh
@@ -0,0 +1,11 @@
+#!/run/current-system/sw/bin/bash
+windowname="$1"
+startcommand="$2"
+wmctrl -a "$windowname"
+if [[ "$?" == "1" ]]
+then
+  bash -c "$startcommand"
+fi
+
+# bash -c "wmctrl -a chrome ; [ "$?" == "1" ] && google-chrome-stable"
+
